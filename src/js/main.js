4e2759ea34d3e7367638435ba53aa2a04fa3be18
Yakup <yakupjoraev@gmail.com>
2022-08-15 17:56:08 +0300

    added files

---
+++ src/js/main.js
@@ -0,0 +1,129 @@
+// Custom scripts
+// Мобильное меню бургер
+function burgerMenu() {
+  const burger = document.querySelector('.burger')
+  const menu = document.querySelector('.menu')
+  const body = document.querySelector('body')
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active')
+      burger.classList.add('active-burger')
+      body.classList.add('locked')
+    } else {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+}
+burgerMenu()
+
+
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
+function fixedNav() {
+  const nav = document.querySelector('nav')
+
+  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
+  const breakpoint = 1
+  if (window.scrollY >= breakpoint) {
+    nav.classList.add('fixed__nav')
+  } else {
+    nav.classList.remove('fixed__nav')
+  }
+}
+window.addEventListener('scroll', fixedNav)
+
+
+let isMobile = {
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
+		let body=document.querySelector('body');
+if(isMobile.any()){
+		body.classList.add('touch');
+		let arrow=document.querySelectorAll('.arrow');
+	for(i=0; i<arrow.length; i++){
+			let thisLink=arrow[i].previousElementSibling;
+			let subMenu=arrow[i].nextElementSibling;
+			let thisArrow=arrow[i];
+
+			thisLink.classList.add('parent');
+		arrow[i].addEventListener('click', function(){
+			subMenu.classList.toggle('open');
+			thisArrow.classList.toggle('active');
+		});
+	}
+}else{
+	body.classList.add('mouse');
+}
+
+
+
+const divs = document.querySelectorAll('.cards__items-show')
+const length = divs.length;
+const btnShow = document.querySelector('.show')
+
+
+function showVisible(visible) {
+  const toShow = Math.min(visible, length);
+
+  for (let i=0; i < length; i++) {
+      divs[i].style.display = i < toShow ? 'block' : 'none';
+  }
+}
+
+let visible = 0;
+showVisible(visible);
+
+btnShow.addEventListener('click', function () {
+  visible = Math.min(visible + 3, length);
+  showVisible(visible);
+});
+
+const swiper = new Swiper('.swiper', {
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+//   // Responsive breakpoints
+ //  breakpoints: {
+
+//   // when window width is >= 320px
+//   320: {
+//     slidesPerView: 3,
+//     spaceBetween: 20
+//   },
+//   // when window width is >= 480px
+//   480: {
+//     slidesPerView: 3,
+//     spaceBetween: 30
+//   },
+//}
+});
+
+
